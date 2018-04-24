a302c53d6e14b8813d5551b75862d3b0a5652f24
GreenCoat <nickscunningham@gmail.com>
2018-04-24 10:14:30 -0500

    Site loading again

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { AddGameContainer, GamesContainer } from './containers';
@@ -5,8 +6,27 @@ import { Home, Archive, Welcome, About, Contact, DisplayGame, NotFound } from '.
 import { withUser, update } from './services/withUser';
 
 class App extends React.Component {
+  componentDidMount() {
+    console.log('App loaded');
+    // this is going to double check that the user is still actually logged in
+    // if the app is reloaded. it's possible that we still have a user in sessionStorage
+    // but the user's session cookie expired.
+    axios.get('/api/auth')
+      .then(res => {
+        // if we get here, the user's session is still good. we'll update the user
+        // to make sure we're using the most recent values just in case
+        update(res.data);
+      })
+      .catch(err => {
+        // if we get a 401 response, that means the user is no longer logged in
+        if (err.response.status === 401) {
+          update(null);
+        }
+      });
+  }
 
   render(){
+    const { user } = this.props;
     return (
     <Router history={hashHistory}>
         <Route path="/" component={Home}>
--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,10 @@ import App from './App';
 // Don't forget to add your API key
 filepicker.setKey('A23FSrXhUQ6SvbdamJguEz');
 
+console.log('Index loaded');
+
 // Our views are rendered inside the #content div
 ReactDOM.render(
-  App,
+  <App />,
   document.getElementById('root')
 );
