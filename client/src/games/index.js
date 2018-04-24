8e4326e9ccd0282bf2b81f05b49b38ecee03759f
GreenCoat <nickscunningham@gmail.com>
2018-04-24 18:58:11 -0500

    Fixed a small typo

--- client/src/games/index.js
+++ client/src/games/index.js
@@ -1,7 +1,7 @@
 import Hangman from './hangman/Hangman';
 import TTT from './ttt/TTT';
 import Snake from './snake/Snake';
-import Pong from './pong/pong';
+import Pong from './pong/Pong';
  
 //Export all games at once
 export {Hangman, TTT, Snake, Pong};
