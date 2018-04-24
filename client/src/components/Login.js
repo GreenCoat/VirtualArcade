114bcdd7ae40b83f1d61cc35808cf985e3f6dc81
GreenCoat <nickscunningham@gmail.com>
2018-04-23 21:54:46 -0500

    Added User schema and index for model export

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
 class Login extends React.Component {
+	constructor(){
+		super();
+
+		this.state = {
+			username: "",
+			password: ""
+		}
+
+		this.handleSubmit = (evt) => {
+			evt.preventDefault();
+			console.log('Form submitted');
+		}
+
+		this.handleChange = (evt) => {
+			let val = evt.target.value;
+
+			this.setState({
+				username: val
+			});
+		}
+	}
 	render(){
 		return (
 			<div>
-				Login
+				<form onSubmit={this.handleSubmit}>
+						<input type="text" value={this.state.username} onChange={this.handleChange}/>
+						<input type="text" value={this.state.password} onChange={this.handleChange}/>
+						<button type='submit'>Login</button>
+				</form>
 			</div>
 		);
 	}
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -9,7 +9,7 @@ export default class Welcome extends PureComponent {
         <h1 className="cover-heading">Welcome</h1>
         <p className="lead">Click on browse to start your journey into the wiki of games that made history.</p>
         <p className="lead">
-          <Link className="btn btn-lg" to="/games">Foo!</Link>
+          <Link className="btn btn-lg" to="/games">Browse!</Link>
           <Login />
         </p>
       </div>
