767b6934f24024819bcb25a385ab43e9765007bf
GreenCoat <nickscunningham@gmail.com>
2018-04-26 14:16:27 -0500

    Ship moving and shooting

--- client/src/games/asteroids/Asteroids.js
+++ client/src/games/asteroids/Asteroids.js
@@ -9,11 +9,12 @@ class Asteroids extends React.Component {
 			px: 200,
 			py: 200,
 			pa: -90,
-			pv: 5,
-			ma: -90,
+			pv: 0,
+			accelerating: false,
 			turnLeft: false,
 			turnRight: false,
-			firing: false
+			firing: false,
+			shots: []
 		}
 
 		this.handleKeyDown = (evt) => {
@@ -22,6 +23,7 @@ class Asteroids extends React.Component {
 			let tl = this.state.turnLeft;
 			let tr = this.state.turnRight;
 			let fg = this.state.firing;
+			let ac = this.state.accelerating;
 
 			if(evt.keyCode == 37 || evt.keyCode == 65)
 				tl = true;
@@ -29,11 +31,14 @@ class Asteroids extends React.Component {
 				tr = true;
 			if(evt.keyCode == 32)
 				fg = true;
+			if(evt.keyCode == 38 || evt.keyCode == 87)
+				ac = true;
 
 			this.setState({
 				turnLeft: tl,
 				turnRight: tr,
-				firing: fg
+				firing: fg,
+				accelerating: ac
 			});
 		}
 
@@ -43,6 +48,7 @@ class Asteroids extends React.Component {
 			let tl = this.state.turnLeft;
 			let tr = this.state.turnRight;
 			let fg = this.state.firing;
+			let ac = this.state.accelerating;
 
 			if(evt.keyCode == 37 || evt.keyCode == 65)
 				tl = false;
@@ -50,16 +56,15 @@ class Asteroids extends React.Component {
 				tr = false;
 			if(evt.keyCode == 32)
 				fg = false;
+			if(evt.keyCode == 38 || evt.keyCode == 87)
+				ac = false;
 
 			this.setState({
 				turnLeft: tl,
 				turnRight: tr,
-				firing: fg
+				firing: fg,
+				accelerating: ac
 			});
-		}
-
-		this.fire = () => {
-			console.log('Pew');
 		}
 
 		this.gameLoop = () => {
@@ -70,22 +75,59 @@ class Asteroids extends React.Component {
 			let tl = this.state.turnLeft;
 			let tr = this.state.turnRight;
 			let fg = this.state.firing;
+			let ac = this.state.accelerating;
+			let shots = this.state.shots;
 
+			//Rotate player if turning
 			if(tl)
-				pa -= 5;
+				pa -= 10;
 			if(tr)
-				pa += 5;
+				pa += 10;
 
+			//Adjust player speed based on acceleration
+			if(ac){
+				pv+=.5;
+				if(pv > 10)
+					pv = 10;
+			} else {
+				pv-=.5;
+				if(pv < 0)
+					pv = 0;
+			}
+
+
+			//Move player, wrap if out of bounds
 			px += pv * Math.cos(pa * Math.PI / 180);
+			if(px > 400)
+				px = 0;
+			if(px < 0)
+				px = 400;
 			py += pv * Math.sin(pa * Math.PI / 180);
+			if(py > 400)
+				py = 0;
+			if(py < 0)
+				py = 400;
 
-			if(fg)
-				console.log('Pew');
+			//Fire bullets
+			if(fg) {
+				shots.push({sx: px, sy: py, sa: pa});
+				console.log(shots);
+			}
+
+			//Update bullet positions
+			for(var i = 0; i < shots.length; i++){
+				shots[i].sx += 15 * Math.cos(shots[i].sa * Math.PI / 180);
+				shots[i].sy += 15 * Math.sin(shots[i].sa * Math.PI / 180);
+				if(shots[i].sx > 400 || shots[i].sx < 0 || shots[i].sy > 400 || shots[i].sy < 0)
+					shots.splice(i, 1);
+			}
 
 			this.setState({
 				px: px,
 				py: py,
-				pa: pa
+				pa: pa,
+				pv: pv,
+				shots: shots
 			}); 
 		}
 	}
@@ -93,7 +135,7 @@ class Asteroids extends React.Component {
 	componentDidMount(){
 		window.addEventListener('keydown', this.handleKeyDown);
 		window.addEventListener('keyup', this.handleKeyUp);
-		let gameLoop = setInterval(this.gameLoop, 1000/15);
+		let gameLoop = setInterval(this.gameLoop, 1000/20);
 		this.setState({gameLoop: gameLoop});
 	}
 
@@ -112,6 +154,7 @@ class Asteroids extends React.Component {
 					px={this.state.px}
 					py={this.state.py}
 					pa={this.state.pa}
+					shots={this.state.shots}
 				/>
 			</div>
 		);
--- client/src/games/asteroids/Display.js
+++ client/src/games/asteroids/Display.js
@@ -22,6 +22,7 @@ class Display extends React.Component {
 		let px = this.props.px;
 		let py = this.props.py;
 		let pa = this.props.pa;
+		let shots = this.props.shots;
 		let sl = this.state.shipL;
 		let ss = this.state.shotSz;
 
@@ -33,14 +34,22 @@ class Display extends React.Component {
 		ctx.fillStyle='black';
 		ctx.fillRect(0, 0, canv.width, canv.height);
 
-		//Draw play
+		//Draw player
 		ctx.strokeStyle='white';
 		ctx.beginPath();
-		ctx.moveTo(px + (sl * Math.cos((pa+20) * Math.PI / 180)), py + (sl * Math.sin((pa+20) * Math.PI / 180)));
+		ctx.moveTo(px + (sl * Math.cos((pa+200) * Math.PI / 180)), py + (sl * Math.sin((pa+200) * Math.PI / 180)));
 		ctx.lineTo(px, py);
-		ctx.lineTo(px + (sl * Math.cos((pa-20) * Math.PI / 180)), py + (sl * Math.sin((pa-20) * Math.PI / 180)));
+		ctx.lineTo(px + (sl * Math.cos((pa-200) * Math.PI / 180)), py + (sl * Math.sin((pa-200) * Math.PI / 180)));
 		ctx.closePath();
 		ctx.stroke();
+
+		//Draw bullets
+		for(var i = 0; i < shots.length; i++){
+			ctx.fillStyle='white';
+			ctx.beginPath();
+			ctx.arc(shots[i].sx, shots[i].sy, ss,0,2*Math.PI);
+			ctx.fill();
+		}
 	}
 
 	render(){
